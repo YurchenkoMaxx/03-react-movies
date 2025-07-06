68b894b4555f7aefc8f03fa78049b5a3682665cc
Max Yurchenko <yurchenko.maksym@gmail.com>
2025-07-06 13:32:25 +0100

    HW44

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/03-react-movies/',
   plugins: [react()],
   build: {
     sourcemap: true,
