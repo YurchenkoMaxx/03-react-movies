fdcf20d09de5c9c08ebc39649b2604da80003f5c
Max Yurchenko <yurchenko.maksym@gmail.com>
2025-07-06 12:15:18 +0100

    HW3

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,47 @@
+import toast from 'react-hot-toast';
+import styles from './SearchBar.module.css';
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+export default function SearchBar({ onSearch }: SearchBarProps) {
+    const handleSubmit = async (formData: FormData): Promise<void> => {
+        const query = formData.get('query') as string;
+        if (!query.trim()) {
+            toast.error('Please enter a search query.');
+            return;
+        }
+        onSearch(query);
+      };
+      
+
+        return (
+            <header className={styles.header}>
+                <div className={styles.container}>
+                    <a
+                        className={styles.link}
+                        href="https://www.themoviedb.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by TMDB
+                    </a>
+                    <form className={styles.form} action={handleSubmit}>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            name="query"
+                            autoComplete="off"
+                            placeholder="Search movies..."
+                            autoFocus
+                        />
+                        <button className={styles.button} type="submit">
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </header>
+        );
+    }
+   
